77879eb1398483e53032f6e97778230cafe28af0
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-03-08 11:42:03 -0300

    created entity Client

---
+++ src/main/java/com/solucaotecnologia/CRUD_Cliente/entities/Client.java
@@ -0,0 +1,76 @@
+package com.solucaotecnologia.CRUD_Cliente.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private Double income;
+
+    private LocalDate birthDate;
+
+    private Integer children;
+
+    public Client() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
