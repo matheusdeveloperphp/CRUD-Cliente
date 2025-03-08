be89c0992b365f6540484d702eed9463b787fcb3
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-03-08 16:43:37 -0300

    endpoint findallPaged

--- src/main/java/com/solucaotecnologia/CRUD_Cliente/controllers/ClientController.java
+++ src/main/java/com/solucaotecnologia/CRUD_Cliente/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.solucaotecnologia.CRUD_Cliente.controllers;
 import com.solucaotecnologia.CRUD_Cliente.dto.ClientDTO;
 import com.solucaotecnologia.CRUD_Cliente.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,12 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO clientDTO = clientService.findById(id);
         return ResponseEntity.ok(clientDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findallPaged(Pageable pageable) {
+        Page<ClientDTO> dto = clientService.findallPaged(pageable);
+        return ResponseEntity.ok(dto);
     }
 
 }
--- src/main/java/com/solucaotecnologia/CRUD_Cliente/services/ClientService.java
+++ src/main/java/com/solucaotecnologia/CRUD_Cliente/services/ClientService.java
@@ -5,6 +5,8 @@ import com.solucaotecnologia.CRUD_Cliente.entities.Client;
 import com.solucaotecnologia.CRUD_Cliente.repositories.ClientRepository;
 import com.solucaotecnologia.CRUD_Cliente.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +24,12 @@ public class ClientService {
         Optional<Client> result = clientRepository.findById(id);
         Client client = result.orElseThrow(()  -> new  ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findallPaged(Pageable pageable) {
+        Page<Client> list = clientRepository.findAll(pageable);
+        return list.map(x -> new ClientDTO(x));
     }
 
 }
