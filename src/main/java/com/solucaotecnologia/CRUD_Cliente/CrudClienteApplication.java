76cb76cd9a6a4f5221be9a3d72a53c8f45223171
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-03-08 10:57:56 -0300

    project started

---
+++ src/main/java/com/solucaotecnologia/CRUD_Cliente/CrudClienteApplication.java
@@ -0,0 +1,13 @@
+package com.solucaotecnologia.CRUD_Cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClienteApplication.class, args);
+	}
+
+}
