3662758ce710212942dec28fbfe6cdcad12e1a24
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-03-09 20:11:43 -0300

    seed bank

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Brown', '32345678901', 4000.0, '1990-01-01', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Alex Green', '32165498766', 5000.0, '1985-01-01', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Bob Grey', '65498732145', 3000.0, '1995-01-01', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Anna White', '98765432102', 6000.0, '1980-01-01', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('David Black', '45678912365', 3500.0, '1992-01-01', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Emma Blue', '78912345698', 4500.0, '1988-01-01', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('John Red', '98765432101', 5500.0, '1982-01-01', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Sophia Yellow', '12336678902', 2000.0, '1998-01-01', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Michael Purple', '32478498767', 7000.0, '1983-01-01', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Olivia Orange', '65498732146', 2500.0, '1993-10-21', 1);
